6eb3452e93017b3cb8f47e9fc5d295f0be85ac71
weapon <357958137@qq.com>
2024-10-31 15:34:41 +0800

    chore: config

--- internal/admin/model/sys_role.go
+++ internal/admin/model/sys_role.go
@@ -9,7 +9,7 @@ type SysRole struct {
 	base.AutoIncrId `xorm:"extends"`
 	Name            string `xorm:"not null comment('名称') unique VARCHAR(30)"`
 	Code            string `xorm:"not null comment('编码') unique VARCHAR(30)"`
-	DataScope       int    `xorm:"not null default 4 comment('数据权限（1：全部数据权限；2：本部门及以下数据权限；3：本部门数据权限；4：仅本人数据权限；5：自定义数据权限）') TINYINT(1)"`
+	DataScope       uint   `xorm:"not null default 4 comment('数据权限（1：全部数据权限；2：本部门及以下数据权限；3：本部门数据权限；4：仅本人数据权限；5：自定义数据权限）') TINYINT(1)"`
 	Description     string `xorm:"comment('描述') VARCHAR(200)"`
 	Sort            int    `xorm:"not null default 999 comment('排序') INT"`
 	Status          uint   `xorm:"not null default 1 comment('状态（1：启用；2：禁用）') UNSIGNED TINYINT"`
